672ce8ac45949b63490836541650012d4148457c
YanMSH <bromazot@ya.ru>
2024-01-28 12:54:14 +0500

    add: read task solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import fs from 'node:fs';
+
 const read = async () => {
-    // Write your code here 
+    const ERR_STRING = 'FS operation failed';
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToRead.txt';
+
+    try {
+         fs.promises.readFile(`${FILE_DEST}/${FILE_NAME}`, { encoding: 'utf8' })            
+            .catch(err => {
+                if (err) {
+                    throw new Error(ERR_STRING);
+                }
+            })
+            .then(fileData => {
+                console.log(fileData);
+         });
+    } catch {
+        throw new Error(ERR_STRING);
+    }
 };
 
 await read();
