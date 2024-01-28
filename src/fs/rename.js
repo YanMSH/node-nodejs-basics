2d0ccf2e8976f4f36af5b71c3fdd9fa0c7a3f43a
YanMSH <bromazot@ya.ru>
2024-01-28 12:40:10 +0500

    add: rename task solution

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs';
+
 const rename = async () => {
-    // Write your code here 
+    const ERR_STRING = 'FS operation failed';
+    const FILE_PATH = 'files';
+    const OLD_FILENAME = 'wrongFilename.txt';
+    const NEW_FILENAME = 'properFilename.md';
+
+    try {
+        const files = await fs.promises.readdir(FILE_PATH);
+        if (files.includes(NEW_FILENAME)) {
+            throw new Error(ERR_STRING);
+        }
+        fs.promises.rename(`${FILE_PATH}/${OLD_FILENAME}`, `${FILE_PATH}/${NEW_FILENAME}`)
+            .catch(err => {
+                if (err) {
+                    throw new Error(ERR_STRING);
+                }
+            });
+    } catch {
+        throw new Error(ERR_STRING);
+    }
 };
 
 await rename();
