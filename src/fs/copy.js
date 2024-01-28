c5345b38598908b56a94d74749287a84d095ed99
YanMSH <bromazot@ya.ru>
2024-01-28 12:24:54 +0500

    add: copy task solution

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs';
+
 const copy = async () => {
-    // Write your code here 
+    const ERR_STRING = 'FS operation failed';
+    const COPY_DEST = 'files_copy';
+    const FILE_DEST = 'files';
+    try {
+        fs.mkdir(COPY_DEST, {}, (err) => {
+            if (err) {
+                throw new Error(ERR_STRING);
+            }
+        });
+         const files = await fs.promises.readdir(FILE_DEST);
+         files.forEach(file => {
+            fs.copyFile(`${FILE_DEST}/${file}`, `${COPY_DEST}/${file}`, fs.constants.COPYFILE_EXCL, err => {
+                if (err) {
+                    throw new Error(ERR_STRING);
+                }
+            });
+         });
+    } catch {
+        throw new Error(ERR_STRING);
+    }
 };
 
 await copy();
