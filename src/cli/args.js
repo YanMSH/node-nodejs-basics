06f6d369db1a4d8e58c385881f2e81744d782429
YanMSH <bromazot@ya.ru>
2024-01-28 13:54:15 +0500

    add: args task solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
-    // Write your code here 
+    let resultString = '';
+    process.argv.forEach((arg, i) => {
+        if (arg.startsWith('--')) {
+            if (process.argv[i + 1]){
+                if (resultString) {
+                    resultString += `, ${arg} is ${process.argv[i + 1]}`
+                } else {
+                    resultString = `${arg} is ${process.argv[i + 1]}`
+                }
+            }
+        }
+    })
+    if (resultString) {
+        console.log(resultString);
+    }
 };
 
 parseArgs();
