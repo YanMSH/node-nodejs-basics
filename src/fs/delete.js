77e33f52172d03fab1dcede5758c5d4e15ad1736
YanMSH <bromazot@ya.ru>
2024-01-28 12:43:34 +0500

    add: delete task solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    const ERR_STRING = 'FS operation failed';
+    const FILE_PATH = 'files';
+    const FILENAME = 'fileToRemove.txt';
+
+    try {
+        const files = await fs.promises.readdir(FILE_PATH);
+        if (!files.includes(FILENAME)) {
+            throw new Error(ERR_STRING);
+        }
+        fs.promises.rm(`${FILE_PATH}/${FILENAME}`)
+            .catch(err => {
+                if (err) {
+                    throw new Error(ERR_STRING);
+                }
+            });
+    } catch {
+        throw new Error(ERR_STRING);
+    }
 };
 
 await remove();
