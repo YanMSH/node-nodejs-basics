4cbeb4bedce723cc445ebc6ecec43854ef1d7f6f
YanMSH <bromazot@ya.ru>
2024-01-28 13:21:12 +0500

    add: env task solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
 const parseEnv = () => {
-    // Write your code here 
+    let resultString = '';
+    for (const variable in process.env) {
+        if (variable.includes('RSS_')) {
+            if (resultString) {
+                resultString += `; ${variable}=${process.env[variable]}`;
+            } else {
+                resultString = `${variable}=${process.env[variable]}`;
+            }
+        }
+    }
+    if (resultString) {
+        console.log(resultString);
+    }
 };
 
 parseEnv();
