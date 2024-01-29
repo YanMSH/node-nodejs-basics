afd5fb19fc2b1906b790508aab1408629480aed4
YanMSH <bromazot@ya.ru>
2024-01-29 20:37:05 +0500

    add: streams/read task solution

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import fs from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToRead.txt';
+
+    const input = fs.createReadStream(`${FILE_DEST}/${FILE_NAME}`);
+
+    input.on('readable', () => {
+        const data = input.read();
+        if (data) {
+            process.stdout.write(data);
+        }
+    });
 };
 
 await read();
