fd7bfe3ccd61321a20fc828bfe2b7e549abbb018
YanMSH <bromazot@ya.ru>
2024-01-29 20:31:23 +0500

    add: calcHash task solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from 'crypto';
+import fs from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToCalculateHashFor.txt';
+
+    const hash = crypto.createHash('sha256');
+    const input = fs.createReadStream(`${FILE_DEST}/${FILE_NAME}`);
+
+    input.on('readable', () => {
+        const data = input.read();
+        if (data) {
+            hash.update(data);
+        } else {
+            console.log(`${hash.digest('hex')}`);
+        }
+    });
 };
 
 await calculateHash();
