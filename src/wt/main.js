0cae0ac840848a89b0965ff79ca24343933c7d4f
YanMSH <bromazot@ya.ru>
2024-01-29 22:19:32 +0500

    add: wt/main task solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import os from 'os';
+import {Worker} from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const coresAmount = os.cpus().length;
+    const promisesArr = [];
+
+    const postToWorker = (index) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', {
+                workerData: index
+            });
+            worker.on("message", msg => resolve({ status: 'resolved', data: msg }));
+            worker.on("error", () => reject({status: 'error', data: null}));
+            worker.on("exit", () => reject({status: 'error', data: null}));
+        });
+    }
+
+    for (let i = 0; i < coresAmount; i++) {
+        promisesArr.push(postToWorker(10 + i));
+    }
+
+    return Promise.all(promisesArr).then( res => console.log(res))
 };
 
 await performCalculations();
