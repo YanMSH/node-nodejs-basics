9f369f58e867d13c331ccd33d958e4ed726e3b64
YanMSH <bromazot@ya.ru>
2024-01-29 21:40:32 +0500

    add: zlib/decompress task solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import fs from "node:fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToCompress.txt';
+    const ARCHIVE_NAME = 'archive.gz';
+
+    fs.createReadStream(`${FILE_DEST}/${ARCHIVE_NAME}`).pipe(zlib.createUnzip())
+        .pipe(fs.createWriteStream(`${FILE_DEST}/${FILE_NAME}`));
 };
 
 await decompress();
