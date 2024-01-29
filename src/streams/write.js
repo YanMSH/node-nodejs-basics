6a88ab5a10e8fa185fff637b3c9e552ecb5b6b05
YanMSH <bromazot@ya.ru>
2024-01-29 20:47:28 +0500

    add: streams/write task solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import fs from "node:fs";
+
 const write = async () => {
-    // Write your code here 
+
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToWrite.txt';
+
+    const output = fs.createWriteStream(`${FILE_DEST}/${FILE_NAME}`);
+
+    process.stdin.pipe(output);
+
+    process.stdin.resume();
 };
 
 await write();
