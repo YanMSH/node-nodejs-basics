3a9df54224efc3b7194002fb70170d8c4c337dc1
YanMSH <bromazot@ya.ru>
2024-01-29 21:40:17 +0500

    add: zlib/compress task solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import fs from "node:fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+    const FILE_DEST = 'files';
+    const FILE_NAME = 'fileToCompress.txt';
+    const ARCHIVE_NAME = 'archive.gz';
+
+    fs.createReadStream(`${FILE_DEST}/${FILE_NAME}`).pipe(zlib.createGzip())
+        .pipe(fs.createWriteStream(`${FILE_DEST}/${ARCHIVE_NAME}`));
 };
 
 await compress();
